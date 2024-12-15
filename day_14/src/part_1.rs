710d3de21fd1f5bdd6076238577770700c3d5cb7
Fraser Orr <OrrFM@cardiff.ac.uk>
2024-12-15 17:30:39 +0000

    base for day 14

--- day_14/src/part_1.rs
+++ day_14/src/part_1.rs
@@ -8,14 +8,14 @@ use regex::Regex;
 
 #[derive(Debug)]
 struct Robot{
-    position:(u64,u64),
+    position:(i64,i64),
     velocity: (i64,i64),
 }
 #[derive(Debug)]
 struct Map{
     robots: Vec<Robot>,
-    width: u64,
-    height: u64,
+    width: i64,
+    height: i64,
 }
 
 impl Map {
@@ -25,6 +25,9 @@ impl Map {
         self.robots.iter_mut().for_each(|robot|{
 
             //find new position
+
+            robot.position= ( robot.position.0+robot.velocity.0 ,robot.position.1+robot.velocity.1);
+            if robot.
 
         });
     }
@@ -61,20 +64,20 @@ fn main() -> Result<(),Box<dyn std::error::Error>>{
     let output: i64 = 0;
     let corrd_regex: Regex =  Regex::new(r"[-]?\d{1,5},[-]?\d{1,5}").unwrap();
 
-    let width: u64 = 11;
-    let height: u64 =7;
+    let width: i64 = 11;
+    let height: i64 =7;
 
 
     let mut game: Map = Map { robots: vec![] ,width: width, height:height};
     
     for line in buf_reader.lines() {
         let content = line.expect("oops file error");
-        let mut tmp_pos:(u64,u64)= (0,0);
+        let mut tmp_pos:(i64,i64)= (0,0);
         let mut tmp_vel:(i64,i64)= (0,0);
         let corrds: Vec<&str> = corrd_regex.find_iter(&content).map(|m| m.as_str()).collect();
         //println!("{:?}", corrds);
         let tmp: Vec<&str> = corrds[0].split(",").collect();
-        tmp_pos = (tmp[0].parse::<u64>().expect("failed to parse input to number"),tmp[1].parse::<u64>().expect("failed to parse input to number"));
+        tmp_pos = (tmp[0].parse::<i64>().expect("failed to parse input to number"),tmp[1].parse::<i64>().expect("failed to parse input to number"));
         let tmp2: Vec<&str> = corrds[1].split(",").collect();
         tmp_vel = (tmp2[0].parse::<i64>().expect("failed to parse input to number"),tmp2[1].parse::<i64>().expect("failed to parse input to number"));
         //println!("{:?} , {:?}",tmp_pos, tmp_vel);
